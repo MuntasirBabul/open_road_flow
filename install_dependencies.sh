b3f93875739e003e67587b3fc47421c95ca150ea
Muntasir Babul <muntasirbabul9@gmail.com>
2025-08-18 12:44:31 +0600

    automatic yes switch added during install

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,43 +3,43 @@
 #########################################################################
 
 sudo apt update
-sudo apt install environment-modules
-sudo apt install build-essential
-sudo apt install pkg-config 
-sudo apt install tcl 
-sudo apt install tcl-dev 
-sudo apt install libreadline-dev 
-sudo apt install bison 
-sudo apt install flex 
-sudo apt install git 
-sudo apt install clang 
-sudo apt install python3 
-sudo apt install python3-dev 
-sudo apt install python3-pip 
-sudo apt install libpython3-dev 
-sudo apt install libffi-dev 
-sudo apt install gcc 
-sudo apt install g++ 
-sudo apt install make
-sudo apt install qtbase5-dev 
-sudo apt install qttools5-dev 
-sudo apt install libqt5xmlpatterns5-dev 
-sudo apt install qtmultimedia5-dev 
-sudo apt install libqt5multimediawidgets5 
-sudo apt install libqt5svg5-dev
-sudo apt install ruby 
-sudo apt install ruby-dev
-sudo apt install libz-dev
-sudo apt install libgit2-dev
-sudo apt install help2man 
-sudo apt install perl 
-sudo apt install g++  # Alternatively, clang
-sudo apt install libgz  # Non-Ubuntu (ignore if gives error)
-sudo apt install libfl2  # Ubuntu only (ignore if gives error)
-sudo apt install libfl-dev  # Ubuntu only (ignore if gives error)
-sudo apt install zlibc zlib1g zlib1g-dev  # Ubuntu only (ignore if gives error)
-sudo apt install ccache  # If present at build, needed for run
-sudo apt install mold  # If present at build, needed for run
-sudo apt install libgoogle-perftools-dev numactl
-sudo apt install autoconf
+sudo apt install -y environment-modules
+sudo apt install -y build-essential
+sudo apt install -y pkg-config 
+sudo apt install -y tcl 
+sudo apt install -y tcl-dev 
+sudo apt install -y libreadline-dev 
+sudo apt install -y bison 
+sudo apt install -y flex 
+sudo apt install -y git 
+sudo apt install -y clang 
+sudo apt install -y python3 
+sudo apt install -y python3-dev 
+sudo apt install -y python3-pip 
+sudo apt install -y libpython3-dev 
+sudo apt install -y libffi-dev 
+sudo apt install -y gcc 
+sudo apt install -y g++ 
+sudo apt install -y make
+sudo apt install -y qtbase5-dev 
+sudo apt install -y qttools5-dev 
+sudo apt install -y libqt5xmlpatterns5-dev 
+sudo apt install -y qtmultimedia5-dev 
+sudo apt install -y libqt5multimediawidgets5 
+sudo apt install -y libqt5svg5-dev
+sudo apt install -y ruby 
+sudo apt install -y ruby-dev
+sudo apt install -y libz-dev
+sudo apt install -y libgit2-dev
+sudo apt install -y help2man 
+sudo apt install -y perl 
+sudo apt install -y g++  # Alternatively, clang
+sudo apt install -y libgz  # Non-Ubuntu (ignore if gives error)
+sudo apt install -y libfl2  # Ubuntu only (ignore if gives error)
+sudo apt install -y libfl-dev  # Ubuntu only (ignore if gives error)
+sudo apt install -y zlibc zlib1g zlib1g-dev  # Ubuntu only (ignore if gives error)
+sudo apt install -y ccache  # If present at build, needed for run
+sudo apt install -y mold  # If present at build, needed for run
+sudo apt install -y libgoogle-perftools-dev numactl
+sudo apt install -y autoconf
 sudo apt update
